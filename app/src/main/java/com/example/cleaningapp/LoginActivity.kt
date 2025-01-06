b6355a4d96eec037434edde9f162148058b34170
syuraihkv <abu.syuraih.al@students.amikom.ac.id>
2025-01-06 09:40:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/cleaningapp/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.example.cleaningapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val txtUsername:EditText = findViewById(R.id.inputEmail)
+        val txtPassword:EditText = findViewById(R.id.inputPassword)
+        val btnLogin:Button = findViewById(R.id.btnLogin)
+        btnLogin.setOnClickListener{
+            if (txtUsername.text.toString().equals(email)&&
+                txtPassword.text.toString().equals(password)){
+                val intent = Intent(this,AccountActivity::class.java)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this,"Login Failed, Check your email and password",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cleaningapp/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.cleaningapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        // Mengatur aksi ketika tombol "Next" ditekan
+        val btnNext: Button =findViewById(R.id.btnNext)
+        btnNext.setOnClickListener {
+            // Ganti "MainActivity" dengan nama activity tujuan Anda
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+    }
+}
