fe3c2c4d7f18e92b545ee12677b10c6a184aa46f
syuraihkv <abu.syuraih.al@students.amikom.ac.id>
2025-01-07 14:42:22 +0700

    hampir bisa di run

--- app/src/main/java/com/example/cleaningapp/HomeActivity.kt
+++ app/src/main/java/com/example/cleaningapp/HomeActivity.kt
@@ -2,7 +2,6 @@ package com.example.cleaningapp
 
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,43 +9,48 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentContainerView
 
 class HomeActivity : AppCompatActivity() {
-    private fun replaceFragment(fragment: Fragment){
-    val fragmentManager = supportFragmentManager
-    val fragmentTrx = fragmentManager.beginTransaction()
-    fragmentTrx.replace(R.id.fragmentContainerView, fragment)
-    fragmentTrx.conmit()
+
+    // Fungsi untuk mengganti fragment
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_home)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        // Mengatur padding untuk mengakomodasi sistem bar
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { view, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            view.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val txtBeranda:TextView = findViewById(R.id.txtBeranda)
-        val txtOrder: TextView = findViewById(R.id.txtOrder)
-        val txtPaket: TextView =findViewById(R.id.txtPaket)
-        val txtPrepaid: TextView = findViewById(R.id.txtPrepaid)
-        val txtAccount:TextView = findViewById(R.id.txtAkun)
-        val fragmentContainer:FragmentContainerView = findViewById(R.id.fragmentContainerView)
 
-        txtAccount.setOnClickListener{
-            replaceFragment(AccountFragment)
+        // Menghubungkan TextView dengan ID dari XML
+        val txtBeranda: TextView = findViewById(R.id.fragmentBeranda)
+        val txtOrder: TextView = findViewById(R.id.fragmentOrder)
+        val txtPaket: TextView = findViewById(R.id.fragmentPaket)
+        val txtPrepaid: TextView = findViewById(R.id.fragmentPrepaid)
+        val txtAccount: TextView = findViewById(R.id.fragmentAccount)
+
+        // Menambahkan click listener untuk setiap TextView
+        txtAccount.setOnClickListener {
+            replaceFragment(AccountFragment())
         }
-        txtBeranda.setOnClickListener{
-            replaceFragment(BerandaFragment)
+        txtBeranda.setOnClickListener {
+            replaceFragment(BerandaFragment())
         }
-        txtPaket.setOnClickListener{
-            replaceFragment(PaketFragment)
+        txtPaket.setOnClickListener {
+            replaceFragment(PaketFragment())
         }
-        txtOrder.setOnClickListener{
-            replaceFragment(OrderFragment)
+        txtOrder.setOnClickListener {
+            replaceFragment(OrderFragment())
         }
-        txtPrepaid.setOnClickListener{
-            replaceFragment(PrepaidFragment)
+        txtPrepaid.setOnClickListener {
+            replaceFragment(PrepaidFragment())
         }
     }
 }
